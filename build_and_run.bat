8aa57c916155efdbaa924b77c95e4844314050a6
scx1332 <sieciech.czajka@golem.network>
2023-03-15 11:26:14 +0100

    fix dev script

--- build_and_run.bat
+++ build_and_run.bat
@@ -1,4 +1,4 @@
-docker-compose build novpn
-docker-compose kill novpn
-docker-compose up -d novpn
-docker-compose logs -f novpn
+docker-compose -f docker-compose-dev.yml build novpn
+docker-compose -f docker-compose-dev.yml kill novpn
+docker-compose -f docker-compose-dev.yml up -d novpn
+docker-compose -f docker-compose-dev.yml logs -f novpn
